9bee751c81a22044082d0dba25525a015f1514ca
Pavan <pavan.kumar@webenza.com>
2025-07-14 13:36:26 +0530

    [14-07]

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react'
-import { Button, HStack } from "@chakra-ui/react"
+
 import Header from "../../components/Header";
 
 const page = () => {
@@ -10,10 +10,9 @@ const page = () => {
      <Header />
     <div className='abt-div mx-auto space-y-4 p-4'>
         <h1 className='text-2xl font-medium text-amber-500'>About us page</h1>
-         <HStack>
-      <Button>Click me ds</Button>
+       
      
-    </HStack>
+ 
         <p>lorem*2 ipsum dolor sit amet consectetur adipisicing elit.
         Quisquam, voluptatum. Quasi, cumque. Lorem ipsum dolor sit amet consectetur adipisicing elit.   
         Quisquam, voluptatum. Quasi, cumque. Lorem ipsum dolor sit amet consectetur adipisicing elit.
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import { Provider } from "@/components/ui/provider";
+
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -15,7 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
      
         <main>
-          <Provider>{children}</Provider>
+         {children}
         </main>
       </body>
     </html>
